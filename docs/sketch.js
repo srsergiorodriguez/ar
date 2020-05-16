5cd8868828eaf03c07a6d93fa07b69d554dccd52
Sergio Rodríguez Gómez <46205341+srsergiorodriguez@users.noreply.github.com>
2020-05-15 21:26:39 -0500

    Add files via upload

---
+++ docs/sketch.js
@@ -0,0 +1,33 @@
+let cnv;
+let plane;
+
+let x = 0;
+function setup() {
+  plane = document.querySelector('#plane');
+  cnv = createCanvas(512,512);
+  frameRate(12);
+  cnv.id('cnv');
+  cnv.parent('#scene');
+  // cnv.hide()
+  colorMode(HSB);
+  background(0,0,0);
+  fill(100,100,100);
+  noStroke();
+  ellipse(width/2,height/2,100,100);
+  imageURL = imageFromCanvas();
+  plane.setAttribute('src', imageURL);
+}
+
+function draw_() {
+  background(0,0,0);
+  x = x > width ? 0 : x + 1;
+  fill(random(255),100,100);
+  ellipse(x, height/2, 100, 100);
+  imageURL = imageFromCanvas();
+  plane.setAttribute('src', imageURL);
+}
+
+function imageFromCanvas() {
+	const canvas = document.getElementById('cnv');
+  return canvas.toDataURL('image/png',0.1);
+}
